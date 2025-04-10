16524ce68aeaef912d2dd4adf777b909be7927e6
pankaj <jhap0384@gmail.com>
2025-04-10 13:57:56 +0530

    Added admin panel

---
+++ lib/sidebar-data.ts
@@ -0,0 +1,8 @@
+import {BarChart3, Cog} from "lucide-react";
+
+export const sidebarData = {
+    navigation: [{
+        title: "Dashboard", url: "/dashboard", icon: BarChart3,
+
+    }, {title: "Services", url: "/services", icon: Cog,}],
+};
